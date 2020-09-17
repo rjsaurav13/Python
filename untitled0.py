da2edb3c46b7a1faa449c44811852ad765ccd46f
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-17 21:13:17 +0530

    onlline class question

---
+++ untitled0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+usn = input("Enter usn:")
+i=1;
+while i<3:
+    
+    print(usn)
+    if i==2:
+        print(usn+" ",end='')
+        
+    i=i+1
+    print(usn)
