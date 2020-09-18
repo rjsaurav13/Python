e183d4eebff5c69896a1b650bc04328938f0d1da
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-18 12:54:40 +0530

    for loop

---
+++ Count_alphabet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+count=0
+
+for i in 'banana':
+    if i=='a':
+        count+=1
+print(count)
---
+++ ForLoop_range.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+for i in range(1,51):
+    if i%5:
+        i+-1
+        print(i)
