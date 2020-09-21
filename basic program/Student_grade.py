ff9edeb14fcfe503edea00139a60211aaeee771f
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-21 23:36:25 +0530

    grade

---
+++ basic program/Student_grade.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+print("Please enter your marks for 5 subjects: ")
+i=0
+sum=0
+while i<5:
+    num=int(input())
+    sum=sum+num
+    i+=1
+sum=(sum/500)*100
+if sum>=85:
+    print("Grade is A:")
+    elif (sum>=75)
+        print("Grade is A:")
+    elif sum>=65
+        print("Grade is A:")
+    elif sum>=55
+        print("Grade is A:")
+    elif sum>=45
+        print("Grade is A:")
+    else:
+        print("Grade is A:")
