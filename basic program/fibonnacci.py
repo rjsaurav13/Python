77c9c6076f34ab0470a5a7df10c0d9d4e7d866f7
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-21 22:16:09 +0530

    fibinacci

---
+++ basic program/fibonnacci.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+x=0
+y=1
+print("enter the length")
+num=int(input())
+sum=0
+i=0
+while i <=num:
+     print(sum)
+     i=i+1
+     x=y
+     y=sum
+     sum=x+y
+   
