62527ab3b920edf8ef85a33feac249258ba3a9c9
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-21 23:59:17 +0530

    Create nested.py

---
+++ basic program/nested.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
