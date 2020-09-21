e82511582ca20142ba61b30320ce40fb728c5f4f
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-22 01:33:37 +0530

    factorial
    using recursion

---
+++ basic program/python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = int(input())
+print("The factorial of", num, "is", factorial(num))
