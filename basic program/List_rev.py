9e10a314050ab9c1740711eaa66c1eea3f1e2780
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-25 12:49:11 +0530

    Create List_rev.py

---
+++ basic program/List_rev.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+cityname = [] 
+for i in range(0,5):
+    city=input()
+    cityname.append(city)
+cityname.reverse()    
+print(cityname)  
