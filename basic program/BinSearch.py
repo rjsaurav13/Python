6435a41275a3478067cbdb6f6c5206df8f76f322
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-01 22:46:53 +0530

    onlineclass

---
+++ basic program/BinSearch.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+def binarySearch(arr,beg,end,item):  
+    if end >= beg:  
+        mid = int((beg+end)/2)  
+        if arr[mid] == item :  
+            return mid+1  
+        elif arr[mid] < item :   
+            return binarySearch(arr,mid+1,end,item)  
+        else:   
+            return binarySearch(arr,beg,mid-1,item)  
+    return -1  
+      
+  
+arr=[1,2,3,4,5,6,7,8,9,10];  
+item = int(input())  
+location = -1;   
+location = binarySearch(arr,0,9,item);  
+if location != -1:   
+    print(location)  
+else:   
+    print("number is not there")  
