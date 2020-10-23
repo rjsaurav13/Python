c769541baebed649177c3a59a488bd69fe9b7057
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-23 19:57:04 +0530

    Create RPS.py

---
+++ basic program/RPS.py
@@ -0,0 +1,22 @@
+def rps():
+    print("Enter \nR-rock\nS-scissor\nP-paper")
+    p1 = input("player 1 enter ")
+    p2 = input("player 2 enter ")
+
+    while True:
+        if p1 == "R" and p2 == "S" or p1 == "S" and p2 == "P" or p1 == "P" and p2 == "R":
+            print("player 1 wins")
+        elif p1 == "S" and p2 == "P" or p1 == "S" and p2 == "R" or p1 == "R" and p2 == "P":
+            print("player 2 wins")
+        else:
+            print("Game tie")
+            break
+
+
+rps()
+
+ch = input("press Y to play again and N to exit: ")
+if ch == 'Y':
+    rps()
+else:
+    print("Exit......")
