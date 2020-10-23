0e7b54456b2c3b9209d996e6d09e680256048355
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-10-23 21:32:31 +0530

    Update RPS.py

--- basic program/RPS.py
+++ basic program/RPS.py
@@ -6,17 +6,18 @@ def rps():
     while True:
         if p1 == "R" and p2 == "S" or p1 == "S" and p2 == "P" or p1 == "P" and p2 == "R":
             print("player 1 wins")
+            break
         elif p1 == "S" and p2 == "P" or p1 == "S" and p2 == "R" or p1 == "R" and p2 == "P":
             print("player 2 wins")
+            break
         else:
             print("Game tie")
             break
 
-
 rps()
-
 ch = input("press Y to play again and N to exit: ")
-if ch == 'Y':
-    rps()
-else:
-    print("Exit......")
+while ch !="N":
+    if ch == 'Y':
+        rps()
+    else:
+        print("Exit......")
