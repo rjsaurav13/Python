685035c2ac9a5fa85b91b6ed76fffd999645925e
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-12-10 21:06:03 +0530

    x

--- basic program/fhandling.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "enter the two files"
-read f1
-read f2
-sort -u $f1 $f2 >d3.txt
-cat d3.txt
-
--- basic program/primetest.py
+++
@@ -1,9 +0,0 @@
-len=int(input())
-for i in range(2,len):
-    for x in range(2,i):
-        if(i%x==0):
-            break
-    else:
-        print(i,end=",")
-
-
